168d2ce96b7a9a02f7efa269ada5406706d44d20
Natarajan <natvasud@cisco.com>
2020-03-15 20:58:48 +0530

    Configured for external tomcat

--- src/main/java/com/epnm/config/EpnmZtpConfigApplication.java
+++ src/main/java/com/epnm/config/EpnmZtpConfigApplication.java
@@ -2,12 +2,19 @@ package com.epnm.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EpnmZtpConfigApplication {
+public class EpnmZtpConfigApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpnmZtpConfigApplication.class, args);
 	}
+	
+//	@Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(EpnmZtpConfigApplication.class);
+//    }
 
 }
--- src/main/java/com/epnm/config/controller/ZTPConfigController.java
+++ src/main/java/com/epnm/config/controller/ZTPConfigController.java
@@ -1,11 +1,19 @@
 package com.epnm.config.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ZTPConfigController {
+	
+	 @GetMapping("/")
+	    public String main() {
+
+	        return "welcome"; //view
+	    }
 	
 	@RequestMapping(value="/getConfig", method=RequestMethod.GET)
 	public String getConfig() {
